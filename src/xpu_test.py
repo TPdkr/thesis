e22232ff1b2d021c612c1c231d6413d382d67989
TPdkr <tpodko@gmail.com>
2026-03-11 14:45:17 +0100

    mainly working on being able to extract features from specific objects in the image

--- src/xpu_test.py
+++ src/xpu_test.py
@@ -1,3 +1,4 @@
 import torch
-print(torch.xpu.is_available())
-print(torch.xpu.device_count())
+print(f"TORCH VERSION: {torch.__version__}")
+print(f"XPU DEVICE IS AVAIALABLE: {torch.xpu.is_available()}")
+print(f"XPU DEVICE COUNT: {torch.xpu.device_count()}")
